39b298bdc62d71a1341ad56c22d1d80ce6a58bc4
adi8900 <adrianszymanski242@gmail.com>
2019-06-30 08:49:50 +0000

    sdm660-common: rootdir:Use 60 swappiness
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -445,7 +445,8 @@ if [ "$ProductName" == "msmnile" ]; then
       configure_zram_parameters
       configure_read_ahead_kb_values
       echo 0 > /proc/sys/vm/page-cluster
-      echo 100 > /proc/sys/vm/swappiness
+      echo 60 > /proc/sys/vm/swappiness
+
 else
     arch_type=`uname -m`
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
@@ -537,9 +538,9 @@ else
     fi
 
     # Set allocstall_threshold to 0 for all targets.
-    # Set swappiness to 100 for all targets
+    # Set swappiness to 60 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
+    echo 60 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
